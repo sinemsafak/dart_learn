ddca2904352fd71e5d65b2880004b134305138c3
Sinem Şafak <safaksm14@gmail.com>
2024-07-29 11:53:06 +0300

    veri tipileri ve değişkenk kavramı tanımı

---
+++ dart/bolum1/veritipleri_degiskenler.dart
@@ -0,0 +1,21 @@
+/* VERİ TİPLERİ VE DEĞİŞKEN KAVRAMI 
+DEĞİŞKEN: Bir işlemi gerçekleştirmeke için yapılması gerekn ilk şey o veriyi hafızaya almaktır. 
+İşlem yapmak istediğimizde de veriyi çağırıp gerekli işlemleri yerine getirmektir. 
+hafızadaki verileri ifade etmke için programlama dillerinde değişkenleri
+kullanırız. Özetle programlama dilinde işlediğimiz verileri bilgisayarın hafızasında tutmak için 
+yapmış olduğumuz tanımlamalardır. 
+
+Tutulan verinin türüne göre farklı veri tipleri vardır. Dart built - in olarak şu tipleri
+içerir: 
+- Number 
+  - int tamsayılar
+  - double ondalıklı sayılar 
+- String metinsel ifadeler 
+- Boolean true/false 
+
+Tüm veri tipleri Object yani nesneldir. 
+
+Var anahtar kelimesi veriable anlamına gelir ve veri türünü belirtmek 
+istemiyorsak kullanırız. 
+Atanan ifadeye göre otomatik olarak türü belirtilir. ,
+*/
