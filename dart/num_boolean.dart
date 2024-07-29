647cd2ce2dd6cad071a542a121770bb136b6ff69
Sinem Şafak <safaksm14@gmail.com>
2024-07-29 13:31:58 +0300

    number ve boolean veri türleri ve null safety ile ilgili kod örnekleri

--- dart/num_boolean.dart
+++ dart/num_boolean.dart
@@ -14,3 +14,34 @@ yapmamız gerekir.
 
 boolean = true/false değerleri alır. 
 */
+void main(List<String> args) {
+  int yas = 34;
+  print(yas);
+  yas=44;
+  print(yas);
+
+  num yil = 1988;
+  print(yil);
+
+  double sayi= 52;
+  print(sayi);
+
+  int kilo = 84.6.toInt(); //kilo ifadesi double tipinde bir değer fakat int türünde bir değişken atamak istiyorsak double veri tipinde olan sayıyı int yapmamız gerek bunun için .toInt() kalıbını kullanıyoruz
+  print(kilo);
+
+  var sayi2 = 56;
+
+  int numara;
+  numara = 5;
+ // print(numara); değişken tanımlandı fakat içine bir değer atanmadığı için bu şekilde yazdırmak hataya neden olacaktır. 
+  print(numara + 9);
+
+  double? s1= null; //illa null değer kullanılmak isteniyorsa veri tipinden sonra ? konulmalıdır. 
+  //null olarak atama yaptığımız için sonradan değer eklemek istediğimizde kod hata verecektir. Bunu önlemek için değer ataması yaptıktan sonra istenilen değer güncellenebilir. 
+  s1=9;
+  print(s1);
+
+  int hexadecimalSayi = 0xAABBCC;
+  print(hexadecimalSayi);
+
+}
