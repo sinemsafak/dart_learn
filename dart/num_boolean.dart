a33c0ded02ed6e4423e24ec2bf1a69c9cd1b0617
Sinem Şafak <safaksm14@gmail.com>
2024-07-29 13:18:47 +0300

    num, double, int arasındaki farklar ve veri tipi dönüşümleri

---
+++ dart/num_boolean.dart
@@ -0,0 +1,16 @@
+/* num: tamsayı veya ondalıklı bir ifadeyi saklayabilir. 
+int= tamsayıları saklarken kullanılan veri türüdür. 
+hexadecimal değerleri de saklayabilir. 0xAABBCC
+
+double= ondalıklı ifadeler için kullanılan veri türüdür. 
+exponents değerli saklayabilir, 1.42e5 
+
+var anahtar kelime ,le de tamsayı veya ondalıklı ifade saklanabilir 
+var olarak int değer atandıysa sonrasında double değer atanamaz 
+
+değişkenlerimize ilk değerleri atamak zorundayız null değer kabul edilmez. 
+null değerlerin kabul edilmesini istiyorsak int?, double?, bool?, gibi farklı değişken tanımları 
+yapmamız gerekir. 
+
+boolean = true/false değerleri alır. 
+*/
