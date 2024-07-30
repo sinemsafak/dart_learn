c9f51014d51e9249a38465f9538e11ea05754f4e
Sinem Şafak <safaksm14@gmail.com>
2024-07-30 11:32:23 +0300

    VSCode launch.json Dosyasından Uygulamayı Çalıştırmak

---
+++ dart/bolum1/string_literals_interpolation.dart
@@ -0,0 +1,15 @@
+/* String: metinsel ifadeler için kullanılan veri türüdür.
+Çift tırnak veya tek tırnak ile belirtilebilir. 
+iki stringi birleştirmek için + operatörü kullanılır. 
+Birden fazla satır süren metinsel ifdeleri birleştirirken + kullanmasak da olur. 
+    'ifade 1'
+    'ifade 2' olarak kullanılabilir. 
+Interpolation: string ifade içinde başka bir string ifadeyi $ifade olarak kullanabiliriz. 
+Böylece + kullanmak gerekmez 
+Eğer ifade birden fazla alana sahipse $(ifade.method) şeklinde kullanılır. 
+Ayrıca interpolation diğer veri türleri için de geçerlidir. 
+ */
+void main(List<String> args) {
+  print("çalıştı");
+
+}
