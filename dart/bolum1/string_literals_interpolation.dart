15c2d7991c67c4c483cd2702c4b5f29e2e08cbed
Sinem Şafak <safaksm14@gmail.com>
2024-07-30 15:33:28 +0300

    String veri tipi ve Interpolation kavramı

--- dart/bolum1/string_literals_interpolation.dart
+++ dart/bolum1/string_literals_interpolation.dart
@@ -6,10 +6,27 @@ Birden fazla satır süren metinsel ifdeleri birleştirirken + kullanmasak da ol
     'ifade 2' olarak kullanılabilir. 
 Interpolation: string ifade içinde başka bir string ifadeyi $ifade olarak kullanabiliriz. 
 Böylece + kullanmak gerekmez 
-Eğer ifade birden fazla alana sahipse $(ifade.method) şeklinde kullanılır. 
+Eğer ifade birden fazla alana sahipse ${ifade.method} şeklinde kullanılır. 
 Ayrıca interpolation diğer veri türleri için de geçerlidir. 
  */
 void main(List<String> args) {
-  print("çalıştı");
+  /* "sinem";  dart bize ifadeleri böyle yazma imkanı sunuyor. fakat bu ifadeleri yazdırmak  
+  56;          istediğimizde bunlara ulaşamayız. bu tek satırlı ifadelere literal denir. 
+  true; */
 
+  String isim = "Emre";
+  String soyIsim = "Kalkan";
+  var kurs = "Dart'ın kullanımı";
+  String kursTanimi = "Dart'ı ve Flutter'ı öğreneceğiz";
+
+
+  print(isim + " " + soyIsim);
+  print("$isim $soyIsim");
+  print("soyadım olan $soyIsim'da bulunan karakter sayısı:" + soyIsim.length.toString());
+  print("Karakter sayısı: ${soyIsim.length}");
+  print("$isim kelimesinde bulunan karakter sayısı: ${isim.length}");
+
+  double en = 10;
+  double boy = 12;
+  print("Eni ${en.toInt()} , boyu ${boy.toInt()} olan dikdörtgenin alanı: ${(boy*en).toInt()} 'dir.");
 }
