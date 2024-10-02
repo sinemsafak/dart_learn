68ab5b4788d6fc0285002dc031941e9cc0b572bc
Sinem Şafak <safaksm14@gmail.com>
2024-10-02 11:12:17 +0300

    kullanıcıdan veri alma işlemleri

--- dart/bolum2/kullanicidan_veri_alma.dart
+++ dart/bolum2/kullanicidan_veri_alma.dart
@@ -7,7 +7,8 @@ void main(List<String> args) {
 
   //kullanıcının konsola yazdığı her ifade metin olarak görünür. 
   print("Yaşınızı giriniz: ");
-  int? yas =int.parse(stdin.readLineSync()!); 
+  int? yas =int.parse(stdin.readLineSync()!); //değeri int değere dönüştürmek için "parse" ifadesini kullanıyoruz
+  //null değer ile ilgili sorunda değerin null olarak bırakılmayacağını belirtmek için ! ifadesi kullanılır. 
   yas=yas+5;
   print("Girilen yas: $yas");
 }
