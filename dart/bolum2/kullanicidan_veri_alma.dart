ac946a0f5d8bba095dad8c20c36cd2dde62ca75f
Sinem Şafak <safaksm14@gmail.com>
2024-10-02 11:11:48 +0300

    kullanıcıdan veri alma işlemleri

---
+++ dart/bolum2/kullanicidan_veri_alma.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  print("Adınızı giriniz");
+  String? isim =stdin.readLineSync(); //kullanıcı bu kısmıı boş geçebilir fakat dart dilinde null değer olamdığı için string'den ssonra ? kullanılması gerekir. 
+  print("Girilen isim: $isim");
+
+  //kullanıcının konsola yazdığı her ifade metin olarak görünür. 
+  print("Yaşınızı giriniz: ");
+  int? yas =int.parse(stdin.readLineSync()!); 
+  yas=yas+5;
+  print("Girilen yas: $yas");
+}
