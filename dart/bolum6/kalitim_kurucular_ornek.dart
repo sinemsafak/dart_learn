8736c25c6851ffa9518928cfc2bcfb80794949d6
Sinem Şafak <safaksm14@gmail.com>
2025-08-11 17:45:49 +0300

    Kalıtımda Kurucu Metot Örnek

---
+++ dart/bolum6/kalitim_kurucular_ornek.dart
@@ -0,0 +1,30 @@
+void main(List<String> args) {
+  Kisi emre =Kisi("emre", 34);
+  emre.kendiniTanit();
+
+  Calisan hasan= Calisan("hasan", 30,3000);
+  hasan.kendiniTanit();
+}
+
+class Kisi{
+  String isim;
+  int yas;
+
+  Kisi(this.isim,this.yas);
+
+  void kendiniTanit(){
+    print("benim adım $isim ve yaşım $yas");
+  }
+}
+
+class Calisan extends Kisi{
+    int maas;
+   Calisan (String name, int age, this.maas):super(name,age);
+
+   @override
+  void kendiniTanit() {
+    super.kendiniTanit();
+    print("maaşım da $maas");
+  }
+    
+}
