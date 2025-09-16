07fe07930f24b4d0ee95598a8009f99219bfd909
Sinem Şafak <safaksm14@gmail.com>
2025-09-16 19:37:32 +0300

    Lexical Closure Kavramı

---
+++ dart/bolum8/lexical_closures.dart
@@ -0,0 +1,27 @@
+/*
+- closure özel bir fonksiyondur, closure ile bir üst kapsamdaki değişkenlerin değerlerini değiştirebiliriz.
+*/
+
+//lexical veriable scope
+void main(List<String> args) {
+  var mainDegiskeni=1;
+
+
+  void a(){
+    var aDegiskeni=2;
+
+    void b(){
+      var bDegiskeni=3;
+      print(aDegiskeni);
+      print(bDegiskeni);
+      print(mainDegiskeni);
+    }
+  }
+  var dondurulenFonksiyon=topla(3);
+  var sonuc= dondurulenFonksiyon(4);
+  print(sonuc);
+}
+
+Function topla(int eleman){
+  return (int deger) => deger*eleman;
+}
