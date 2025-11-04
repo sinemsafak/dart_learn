40b8287906750b696291b12f67d6086ede8674b2
Sinem Şafak <safaksm14@gmail.com>
2025-11-04 14:11:47 +0300

    then, catchError ve whenComplete Metotları kısa örneklendirilmesi

--- dart/bolum10/asekron_programlama.dart
+++ dart/bolum10/asekron_programlama.dart
@@ -1,15 +1,19 @@
 
 void main(List<String> args) {
   print("anne çocuğu ekmek alamaya yollar");
-  uzunSurenIslem();
+  uzunSurenIslem()
+  .then((value)=>print(value))
+  .catchError((hata)=>print(hata))
+  .whenComplete(()=>print("ekmek alma işlemi bitti"));
+
   print("peynir zeyitin hazrılar ");
   print("kahvaltı hazır");
 }
 
-void uzunSurenIslem() {
-  print("çocuk ekmek almak için evden çıkar");
- Future.delayed(Duration(seconds:5),(){
-  print("çocuk ekmekle eve girer");
+Future<String> uzunSurenIslem() {
+  //print("çocuk ekmek almak için evden çıkar");
+ return Future<String>.delayed(Duration(seconds:5),(){
+  return "çocuk ekmekle eve girer";
  });
   
 }
